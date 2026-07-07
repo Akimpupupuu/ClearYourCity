16410606ab176a68836fc1dcdef20ddd5bdf7de6
Akimpupupuu <dnsgon3@gmail.com>
2026-07-07 12:27:44 +0400

    environment for auth service

---
+++ auth-service/migrations/000001_init.up.sql
@@ -0,0 +1,23 @@
+CREATE SCHEMA auth_service;
+
+CREATE TABLE IF NOT EXISTS auth_service.users (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    version BIGINT NOT NULL DEFAULT 1,
+    full_name VARCHAR(100) NOT NULL CHECK(char_length(full_name) BETWEEN 3 AND 100),
+    email VARCHAR(100) NOT NULL UNIQUE CHECK(
+        email ~ '^.+@.+\..+$'
+        AND
+        char_length(email) BETWEEN 5 AND 100
+    ),
+    hashed_password TEXT NOT NULL,
+    created_at TIMESTAMPTZ
+);
+
+CREATE TABLE IF NOT EXISTS auth_service.sessions (
+    id UUID PRIMARY KEY NOT NULL,
+    user_id BIGINT NOT NULL REFERENCES auth_service.users(id) ON DELETE CASCADE,
+    refresh_token TEXT NOT NULL,
+    is_revoked BOOLEAN NOT NULL DEFAULT false,
+    created_at TIMESTAMPTZ NOT NULL,
+    expires_at TIMESTAMPTZ NOT NULL
+);
